eaacff0f76550a0ed45a651435b182b3b05d60f4
marina <nicolaimarina@gmail.com>
2021-03-16 16:08:28 -0400

    creating the new id

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidv4 } from 'uuid'
 
 export default function Login({ onIdSubmit }) {
     const idRef = useRef()
@@ -10,6 +11,10 @@ export default function Login({ onIdSubmit }) {
         onIdSubmit(idRef.current.value)
     }
 
+    function createNewId() {
+        onIdSubmit(uuidv4())
+    }
+
     return (
         <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
             <Form onSubmit={handleSubmit} className='w-100'>
@@ -18,7 +23,7 @@ export default function Login({ onIdSubmit }) {
                     <Form.Control type="text" ref={idRef} required></Form.Control>
                 </Form.Group>
                 <Button type="submit" className="mr-2">Login</Button>
-                <Button variant="secondary">Create a New Id</Button>
+                <Button onClick={createNewId} variant="secondary">Create a New Id</Button>
             </Form>
         </Container>
     )
